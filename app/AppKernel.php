0a9a841bb868b1f73fe626d0a668d10de7be1c7a
Tristan Roussel <troussel@phare.normalesup.org>
2015-06-10 22:57:24 +0200

    Add L10nBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new L10nBundle\L10nBundle(),
             new AppBundle\AppBundle(),
         );
 
@@ -24,7 +25,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            $bundles[] = new \Fansible\DevopsBundle\FansibleDevopsBundle();
+            $bundles[] = new Fansible\DevopsBundle\FansibleDevopsBundle();
         }
 
         return $bundles;
